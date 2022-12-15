5c77c486a7a1b1711d05e037ecaa3b917af21a6a
KhantNyarKo <cgm.khantnyarko@gmail.com>
2022-12-15 09:42:46 +0630

    bugs fixed

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,21 +4,5 @@ $(document).ready(function () {
         speed: 1000,
         slidesToShow: 2,
         variableWidth: true,
-        responsive: [
-            {
-                breakpoint: 1280,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            }
-        ]
     });
 });
